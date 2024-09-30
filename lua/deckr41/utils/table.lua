e233bd56350c259b87548fba558c11a7b6df45f2
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-09-30 14:06:53 +0200

    refactor(utils): simplify and polish table utility functions
    - Add comments block to all functions
    - Use consistent terms and variable naming
    - Add `values`, `prepend`, `pick`, `map`, and `imap` utility functions
    - Remove redundant and unused functions

--- lua/deckr41/utils/table.lua
+++ lua/deckr41/utils/table.lua
@@ -3,76 +3,58 @@ local Logger = require("deckr41.utils.logger") --- @class Logger
 --- @class TableUtils
 local M = {}
 
--- Function to concatenate arrays with a predicate callback
---- @param a table First array
---- @param b table Second array
---- @param predicate_callback function Predicate function that takes an item and seen_ids table
---- @return table Concatenated array
-M.concat_with = function(a, b, predicate_callback)
-  local result = vim.deepcopy(a)
-  local seen_ids = {}
+--- Extract the values from a dictionary (named table) and return them as
+--- an array (indexed table)
+--- @generic T
+--- @param dict table<any, T>
+--- @return T[]
+M.values = function(dict)
+  local result_array = {}
+  for _, value in pairs(dict) do
+    result_array[#result_array + 1] = value -- More efficient than table.insert in a loop
+  end
+  return result_array
+end
 
-  for _, item in ipairs(a) do
-    seen_ids[item.id] = true
+--- Concatenate arrays (indexed tables) with a decider function
+--- @generic T
+--- @param array_a T[] First array
+--- @param array_b T[] Second array
+--- @param opts { should_deepcopy?: boolean, fn: fun(item_a: T, item_b: T): T|nil }
+--- @return T[]: Array with concatenated items from both arrays
+M.concat_with = function(array_a, array_b, opts)
+  local result_array = {}
+  -- Copying tbl_a items into a dictionary (named table) for easier lookups
+  for _, item_a in ipairs(array_a) do
+    result_array[item_a.id] = opts.should_deepcopy and vim.deepcopy(item_a)
+      or item_a
   end
 
-  for _, item in ipairs(b) do
-    if predicate_callback(item, seen_ids) then
-      result[item.id] = item
-      seen_ids[item.id] = true
+  for _, item_b in ipairs(array_b) do
+    local existing_item = result_array[item_b.id]
+    local new_item_b = opts.should_deepcopy and vim.deepcopy(item_b) or item_b
+
+    if existing_item then
+      local negociated_item = opts.fn(existing_item, new_item_b)
+      if negociated_item then result_array[item_b.id] = negociated_item end
     else
-      table.insert(result, item)
+      result_array[item_b.id] = new_item_b
     end
   end
 
-  return result
+  return M.values(result_array)
 end
 
--- Merge two tables
---- @param a table First table
---- @param b table Second table
---- @return table Merged table
-M.merge = function(a, b)
-  local result = vim.deepcopy(a)
-  for k, v in pairs(b) do
-    result[k] = v
-  end
-  return result
-end
-
---- Extend a table, `target`, with data from another, `extra`.
---- @param target table First table
---- @param extra table Second table
---- @param opts ?{ ignore_nil?: boolean, mutate?: boolean }
---- @return table: Extended `target` table. If `mutate` is false (default)
-M.deep_extend = function(target, extra, opts)
-  opts = opts or {}
-  local ignore_nil = opts.ignore_nil or true
-  local mutate = opts.mutate or false
-  local result = mutate and target or vim.tbl_deep_extend("force", {}, target)
-
-  for k, v in pairs(extra) do
-    if v == nil and not ignore_nil then
-      result[k] = nil
-    elseif type(v) == "table" and type(result[k] or false) == "table" then
-      result[k] = M.deep_extend(result[k], v)
-    else
-      result[k] = v
-    end
-  end
-  return result
-end
-
---- Get a property deep in a table without triggering an error.
---- @param input table|nil
+--- Get a property deep in a table without triggering an error
+--- @param tbl table|nil
 --- @return any|nil
-M.deep_get = function(input, ...)
-  if type(input) ~= "table" then return nil end
+M.deep_get = function(tbl, ...)
+  if type(tbl) ~= "table" then return nil end
 
   local keys = { ... }
   if #keys == 0 then return nil end
 
-  local result = input
+  local result = tbl
   for _, value in ipairs(keys) do
     if result[value] == nil then return nil end
     result = result[value]
@@ -81,39 +63,53 @@ M.deep_get = function(input, ...)
   return result
 end
 
---- Extract the values from a named table and return them as an indexed table
+--- Add an item at the beginning of an array (indexed table)
 --- @generic T
---- @param input table<any, T>
---- @return T[]
-M.values = function(input)
-  local result = {}
-  for _, v in pairs(input) do
-    table.insert(result, v)
-  end
-  return result
+--- @param array T[] The array to which the item will be prepended
+--- @param item T The item to prepend to the array
+--- @return T[]: The mutated array
+M.prepend = function(array, item)
+  table.insert(array, 1, item)
+  return array
 end
 
---- Add an item at the beginning of a table
---- @generic T
---- @param list T[]: The list to which the item will be prepended
---- @param item T: The item to prepend to the list
---- @return T[]: The modified/mutated list
-M.prepend = function(list, item)
-  table.insert(list, 1, item)
-  return list
-end
-
---- Pick a set of fields from a given table
---- @generic T: table
---- @param list T
---- @param fields string[]
---- @return T
-M.pick = function(list, fields)
-  local result = {}
+--- Pick a set of fields from a dictionary (named table)
+--- @generic V
+--- @param dict table<string, V>
+--- @param fields string[] Array (indexed table) of fields/keys to pick from the dictionary
+--- @return table<string, V>: The dictionary with only the specified fields picked
+M.pick = function(dict, fields)
+  local result_dict = {}
   for _, field in ipairs(fields) do
-    result[field] = list[field]
+    result_dict[field] = dict[field]
   end
-  return result
+  return result_dict
+end
+
+--- Map over a dictionary (named table) and transform its values
+--- @generic K, V, M
+--- @param dict table<K, V>
+--- @param fn fun(value: V, key: K): M
+--- @return table<K, M>
+M.map = function(dict, fn)
+  local result_dict = {}
+  for key, value in pairs(dict) do
+    result_dict[key] = fn(value, key)
+  end
+  return result_dict
+end
+
+--- Map over an array (indexed table) and transform its items
+--- @generic K, V, M
+--- @param array table<K, V>
+--- @param func fun(item: V, index: K): M
+--- @return M[]
+M.imap = function(array, func)
+  local result_array = {}
+  for index, item in ipairs(array) do
+    result_array[index] = func(item, index)
+  end
+  return result_array
 end
 
 return M
