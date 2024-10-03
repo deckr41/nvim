b16d8aaf0838512337507d6dd1e320ca73d54a0a
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-10-03 11:34:24 +0200

    refactor(utils): improve naming consistency in `array.lua`

--- lua/deckr41/utils/array.lua
+++ lua/deckr41/utils/array.lua
@@ -1,40 +1,46 @@
 --- @class ArrayUtils
 local M = {}
 
+--- Find the first element in an array (indexed table) that satisfies
+--- the predicate function.
 --- @generic T
---- @param input T[]
---- @param predicate fun(item: T):boolean
+--- @param array T[]
+--- @param predicate fun(item: T): boolean
 --- @return T|nil
-M.find_first = function(input, predicate)
-  for _, value in ipairs(input) do
+M.find_first = function(array, predicate)
+  for _, value in ipairs(array) do
     if predicate(value) then return value end
   end
   return nil
 end
 
---- @param list string|string[]
+--- Join the elements of an array (indexed table) into a single string,
+--- using the specified separator.
+--- @param array string|string[]
 --- @param separator ?string
 --- @return string
-M.join = function(list, separator)
+M.join = function(array, separator)
   separator = separator or "\n"
-  if type(list) == "table" then return table.concat(list, separator) end
-  return list
+  if type(array) == "table" then return table.concat(array, separator) end
+  return array
 end
 
----
+--- Combine multiple arrays (indexed tables) into a single array.
 --- @generic T
---- @param a T[]
---- @param b T[]
+--- @param array_a T[]
+--- @param array_b T[]
 --- @return T[]
-M.concat = function(a, b)
-  local result = {}
-  for _, v in ipairs(a) do
-    table.insert(result, v)
+M.concat = function(array_a, array_b)
+  local result_array = {}
+
+  for _, v in ipairs(array_a) do
+    result_array[#result_array + 1] = v
   end
-  for _, v in ipairs(b) do
-    table.insert(result, v)
+  for _, v in ipairs(array_b) do
+    result_array[#result_array + 1] = v
   end
-  return result
+
+  return result_array
 end
 
 return M
