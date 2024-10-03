e27089f514f0f8f67ea725e450b065fb367cba41
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-10-03 13:13:01 +0200

    feat(utils): add `get_first` function to `array.lua`
    - Introduce `get_first` to retrieve the first matching element or throw
      an error if not found, complementing `find_first` which return `nil`

--- lua/deckr41/utils/array.lua
+++ lua/deckr41/utils/array.lua
@@ -2,16 +2,31 @@
 local M = {}
 
 --- Find the first element in an array (indexed table) that satisfies
---- the predicate function.
+--- the `is_the_one` predicate function.
 --- @generic T
---- @param array T[]
---- @param predicate fun(item: T): boolean
---- @return T|nil
-M.find_first = function(array, predicate)
-  for _, value in ipairs(array) do
-    if predicate(value) then return value end
+--- @param haystack_array T[] The array to iterate over.
+--- @param is_the_one fun(needle: T): boolean A function that checks if an element is found.
+--- @return T|nil: The first element found, otherwise `nil`.
+M.find_first = function(haystack_array, is_the_one)
+  for _, straw in ipairs(haystack_array) do
+    if is_the_one(straw) then return straw end
   end
   return nil
+end
+
+--- Get the first element in an array (indexed table) that satisfies
+--- the `is_the_one` predicate function.
+---
+--- Similar to `find_first`, but throws an error if no element found.
+--- @generic T
+--- @param haystack_array T[] The array to iterate over.
+--- @param is_the_one fun(needle: T): boolean A function that checks if an element is found.
+--- @return T: The first element found, otherwise throw.
+M.get_first = function(haystack_array, is_the_one)
+  for _, straw in ipairs(haystack_array) do
+    if is_the_one(straw) then return straw end
+  end
+  error("Element not found")
 end
 
 --- Join the elements of an array (indexed table) into a single string,
