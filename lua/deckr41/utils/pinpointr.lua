a0b7060636b53702859cb9026f374125d3dafe42
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-10-24 10:42:52 +0200

    feat(utils): add `pinpointr.lua` for tracking user state in files

---
+++ lua/deckr41/utils/pinpointr.lua
@@ -0,0 +1,40 @@
+--- @class Pinpointr
+local M = {}
+
+--- @class PinpointrState User's current state in the file
+local state = {
+  win_id = nil, --- @type integer?
+  cursor = nil, --- @type integer[]?
+  range = nil, --- @type integer[]?
+  filetype = nil, --- @type string?
+}
+
+--- Update the user's whereabouts
+--- @param opts { range?: integer[] }?
+--- @return PinpointrState
+M.take_snapshot = function(opts)
+  opts = opts or {}
+  local buf_id = vim.api.nvim_get_current_buf()
+  local win_id = vim.api.nvim_get_current_win()
+
+  state.win_id = win_id
+  state.cursor = vim.api.nvim_win_get_cursor(win_id)
+  state.range = opts.range
+  state.filetype = vim.bo[buf_id].filetype
+
+  return state
+end
+
+M.get = function() return vim.deepcopy(state) end
+
+M.get_position = function()
+  return {
+    win_id = state.win_id,
+    cursor = state.cursor,
+    range = state.range,
+  }
+end
+
+M.get_filetype = function() return state.filetype end
+
+return M
