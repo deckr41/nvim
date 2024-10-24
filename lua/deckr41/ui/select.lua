00102ecc71e48445df221e3ecd7d2e2239cb89df
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-10-24 12:43:53 +0200

    refactor(ui): improve `select.lua` with simplified rendering and preview logic
    - Refactor `select.lua` to integrate with `TextareaUI` for item previews
    - Simplify rendering logic and enhance item selection handling

--- lua/deckr41/ui/select.lua
+++ lua/deckr41/ui/select.lua
@@ -1,12 +1,13 @@
-local Logger = require("deckr41.utils.logger")
+-- width
+-- width
+local Logger = require("deckr41.utils.loggr")("SelectUI")
 local NVimUtils = require("deckr41.utils.nvim")
 local TableUtils = require("deckr41.utils.table")
+local TextareaUI = require("deckr41.ui.textarea")
 
 --- Custom highlight style
---- Define your highlight group using already defined theme color
 local string_hl_group =
   vim.api.nvim_get_hl(0, { name = "String", link = false })
-
 vim.api.nvim_set_hl(0, "D41SelectUISelected", {
   fg = string_hl_group.fg or "#00FF00",
 })
@@ -14,94 +15,19 @@ vim.api.nvim_set_hl(0, "D41SelectUISelected", {
 --- @class SelectUIItem
 --- @field id string
 --- @field group_id? string
---- @field text string
+--- @field label? string
 --- @field is_separator? boolean
 --- @field is_selected? boolean
+--- @field preview? string|(fun():string)|TextareaUITab[]
 
 --- @class SelectUIItemGroup
 --- @field id string
---- @field text string
+--- @field label? string
 --- @field items SelectUIItem[]
---- @field selected string
+--- @field selected? string
 
 --- @class SelectUI
-local SelectUI = {}
-
---- Calculate the width of the window based on the longest item
---- @param items SelectUIItem[]
---- @return number
-local max_line_width = function(items)
-  return TableUtils.max_with(
-    items,
-    function(item) return vim.fn.strdisplaywidth(item.text) end
-  ) or 0
-end
-
---- Render the items in the buffer
---- @param config SelectUIConfig
---- @param state SelectUIState
-local render = function(config, state)
-  if not NVimUtils.is_buf_valid(state.buf_id) then
-    Logger.error("Buffer ID is invalid", { buf_id = state.buf_id })
-    return
-  end
-
-  vim.api.nvim_set_option_value("modifiable", true, { buf = state.buf_id })
-  vim.api.nvim_buf_set_lines(state.buf_id, 0, -1, false, {})
-
-  for idx, item in ipairs(state.items) do
-    local line = item.text
-    local line_index = idx - 1
-
-    vim.api.nvim_buf_set_lines(
-      state.buf_id,
-      line_index,
-      line_index,
-      false,
-      { line }
-    )
-
-    -- Apply separator highlight
-    local line_heighlight = nil
-
-    if item.is_separator then line_heighlight = config.highlights.separator end
-
-    if item.is_selected then line_heighlight = config.highlights.selected end
-
-    if line_heighlight then
-      vim.api.nvim_buf_add_highlight(
-        state.buf_id,
-        -1,
-        line_heighlight,
-        line_index,
-        0,
-        -1
-      )
-    end
-  end
-
-  -- Refresh dimensions in case items changed
-  local width = max_line_width(state.items) + 1 -- right padding
-  local height = math.min(#state.items, config.max_height)
-
-  vim.api.nvim_win_set_config(state.win_id, {
-    width = width,
-    height = height,
-  })
-
-  -- Freeze it back after updating the content
-  vim.api.nvim_set_option_value("modifiable", false, { buf = state.buf_id })
-
-  -- Set cursor to the current item position
-  vim.api.nvim_win_set_cursor(state.win_id, { state.cursor_pos, 0 })
-
-  -- Ensure the window's view starts from the first line
-  vim.api.nvim_win_call(state.win_id, function()
-    local view = vim.fn.winsaveview()
-    view.topline = 1 -- Set the top visible line to the first line
-    vim.fn.winrestview(view)
-  end)
-end
+local M = {}
 
 --- Build render-ready items from groups
 --- @param groups SelectUIItemGroup[]
@@ -113,7 +39,7 @@ local build_items_from_groups = function(groups)
   for _, group in ipairs(groups) do
     result_array[index] = {
       id = group.id,
-      text = "[" .. (group.text or group.id) .. "]",
+      label = group.label or group.id,
       is_separator = true,
     }
     index = index + 1
@@ -124,8 +50,9 @@ local build_items_from_groups = function(groups)
       result_array[index] = {
         id = item.id,
         group_id = group.id,
-        text = marker .. item.text,
+        label = marker .. (item.label or item.id),
         is_selected = is_selected,
+        preview = item.preview,
       }
       index = index + 1
     end
@@ -162,176 +89,333 @@ local move_cursor = function(delta, opts)
   return new_pos
 end
 
---- @class SelectUIBuildOpts
---- @field items SelectUIItemGroup[]
---- @field max_height? number
---- @field max_width? number
+--- @class SelectUIHighlights
+--- @field normal string?
+--- @field cursorline string?
+--- @field separator string?
+--- @field title string?
+--- @field selected string?
+
+--- @class SelectUIConfig
+--- @field groups SelectUIItemGroup[]?
+--- @field max_width? integer
+--- @field max_height? integer
 --- @field title? string
---- @field title_pos? "left"|"center"|"right"
---- @field highlights? { normal?: string, cursorline?: string, separator?: string, title?: string }
---- @field should_close_on_change? boolean
---- @field on_change? fun(self: SelectUIInstance, item: SelectUIItem)
+--- @field title_pos? "left"|"right"|"center"
+--- @field highlights SelectUIHighlights?
+--- @field win_opts vim.api.keyset.win_config? Window options
+--- @field close_on_select boolean?
+--- @field on_open function?
+--- @field on_close  function?
+--- @field on_select fun(self: SelectUIInstance, item: SelectUIItem)?
+--- @field preview_config TextareaUIConfig?
 
---- Build a new select UI instance
---- @param opts SelectUIBuildOpts
---- @return SelectUIInstance|nil
-SelectUI.build = function(opts)
-  if not opts.items or type(opts.items) ~= "table" then
-    Logger.error(
-      "Cannot create instance with invalid items parameter",
-      { items = opts.items }
-    )
-    return
-  end
-
+--- Build a new SelectUI instance.
+--- @param user_config SelectUIConfig
+--- @return SelectUIInstance
+M.build = function(user_config)
   --- @class SelectUIInstance
   local instance = {}
 
-  --- @class SelectUIConfig
+  --- @type SelectUIConfig
   local config = {
-    max_height = opts.max_height or 50,
-    max_width = opts.max_width or 20,
-    title = opts.title,
-    title_pos = opts.title_pos or "center",
-    highlights = vim.tbl_deep_extend("force", {
+    groups = {},
+    max_width = 20,
+    max_height = 50,
+    title = nil,
+    title_pos = "center",
+    highlights = {
       normal = "Normal",
       cursorline = "CursorLine",
       separator = "Comment",
       selected = "D41SelectUISelected",
       title = "Normal",
-    }, opts.highlights or {}),
-    should_close_on_change = opts.should_close_on_change or false,
-    --- @type fun(self: SelectUIInstance, item: SelectUIItem)
-    on_change = opts.on_change,
+    },
+    win_opts = {
+      relative = "cursor",
+      width = 20,
+      height = 50,
+      row = -1,
+      col = 0,
+      style = "minimal",
+      border = "rounded",
+      noautocmd = true,
+    },
+    close_on_select = false,
+    on_open = nil,
+    on_close = nil,
+    on_select = nil,
+    preview_config = {
+      title = " Preview ",
+      win_opts = {
+        relative = "win",
+        anchor = "NW",
+        focusable = true, -- For scrolling to work
+      },
+    },
   }
+
+  -- Merge user options into the default config
+  config = vim.tbl_deep_extend("force", config, user_config)
 
   --- @class SelectUIState
   local state = {
-    win_id = nil,
-    buf_id = nil,
-    --- @type SelectUIItem[]
-    items = build_items_from_groups(opts.items or {}),
-    cursor_pos = 0,
+    buf_id = nil, --- @type integer? Buffer id
+    win_id = nil, --- @type integer? Window id
+    is_visible = false, --- @type boolean Is the UI currently visible
+    items = build_items_from_groups(config.groups or {}),
+    cursor_pos = 0, --- @type integer
+    preview_ui = nil, --- @type TextareaUIInstance?
   }
 
+  --- Update the preview window based on the selected item
+  local function render_preview()
+    if not state.is_visible then return end
+
+    local item = state.items[state.cursor_pos]
+    if not item or item.is_separator or not item.preview then
+      -- New selected hovered item does not have a preview, close current pane
+      if state.preview_ui then
+        state.preview_ui.hide()
+        state.preview_ui = nil
+      end
+      return
+    end
+
+    if not state.preview_ui then
+      state.preview_ui = TextareaUI.build(
+        vim.tbl_deep_extend("force", config.preview_config or {}, {
+          value = item.preview,
+          win_opts = {
+            win = state.win_id,
+            row = -1,
+            col = vim.api.nvim_win_get_width(state.win_id) + 1,
+          },
+        })
+      )
+    else
+      state.preview_ui.update({ value = item.preview })
+    end
+
+    state.preview_ui.show()
+  end
+
+  --- Move the cursor up or down
   --- @param direction "up"|"down"
-  local move = function(direction)
-    state.cursor_pos = move_cursor(direction == "up" and -1 or 1, {
+  local function move(direction)
+    local new_position = move_cursor(direction == "up" and -1 or 1, {
       items = state.items,
       cursor_pos = state.cursor_pos,
       should_cycle = false,
     })
-    if NVimUtils.is_win_valid(state.win_id) then
+
+    if new_position == state.cursor_pos then return end
+
+    state.cursor_pos = new_position
+    if state.is_visible then
       vim.api.nvim_win_set_cursor(state.win_id, { state.cursor_pos, 0 })
+      render_preview()
     end
   end
 
-  -- Ensure cursor starts at the correct item position, considering separators
-  move("down")
-
-  --- Open the select UI
-  instance.open = vim.schedule_wrap(function()
-    if NVimUtils.is_win_valid(state.win_id) then
-      Logger.warn("SelectUI already open", { win_id = state.win_id })
-      return
-    end
-    if #state.items == 0 then
-      Logger.warn(
-        "SelectUI cannot be opened with no items",
-        { items = state.items }
-      )
-      return
-    end
-
-    -- Create a new buffer
-    state.buf_id = NVimUtils.create_scratch_buffer({
-      buftype = "nofile",
-      bufhidden = "wipe",
-      buflisted = false,
-      swapfile = false,
-      modifiable = false,
-      filetype = "d41_ui_select",
+  --- Handle keymaps for the select UI
+  local function setup_keymaps()
+    NVimUtils.add_keymaps({
+      buf_id = state.buf_id,
+      modes = {
+        n = {
+          ["<CR>"] = instance.select_item,
+          ["<Esc>"] = instance.hide,
+          ["j"] = function() move("down") end,
+          ["<Down>"] = function() move("down") end,
+          ["k"] = function() move("up") end,
+          ["<Up>"] = function() move("up") end,
+          ["<Tab>"] = function()
+            if state.preview_ui then state.preview_ui.switch_next_tab() end
+          end,
+          ["<S-Tab>"] = function()
+            if state.preview_ui then state.preview_ui.switch_prev_tab() end
+          end,
+        },
+      },
     })
+  end
 
-    -- Calculate window size and position
-    local width = max_line_width(state.items) + 1 -- right padding
-    local height = math.min(#state.items, config.max_height)
+  ---@return integer width
+  ---@return integer height
+  local function calculate_dimensions()
+    local max_label_length = TableUtils.max_with(
+      state.items,
+      function(item) return vim.fn.strdisplaywidth(item.label or "") end
+    ) or 0
+    local item_count = #state.items
 
-    -- Create a floating window at the cursor position
-    state.win_id = vim.api.nvim_open_win(state.buf_id, true, {
-      relative = "cursor",
-      row = -1,
-      col = 0,
-      title = config.title,
-      title_pos = config.title_pos,
+    return
+      -- width
+      math.max(max_label_length + 1, vim.fn.strdisplaywidth(config.title)),
+      -- height
+      math.min(item_count ~= 0 and item_count or 2, config.max_height)
+  end
+
+  --- Render the items in the buffer
+  local render = function()
+    if not state.is_visible then return end
+
+    vim.api.nvim_set_option_value("modifiable", true, { buf = state.buf_id })
+    vim.api.nvim_buf_set_lines(state.buf_id, 0, -1, false, {})
+
+    for index, item in ipairs(state.items) do
+      local line = item.label
+      local line_index = index - 1
+
+      vim.api.nvim_buf_set_lines(
+        state.buf_id,
+        line_index,
+        line_index,
+        false,
+        { line }
+      )
+
+      -- Apply separator highlight
+      local line_highlight = nil
+      if item.is_separator then line_highlight = config.highlights.separator end
+      if item.is_selected then line_highlight = config.highlights.selected end
+
+      if line_highlight then
+        vim.api.nvim_buf_add_highlight(
+          state.buf_id,
+          -1,
+          line_highlight,
+          line_index,
+          0,
+          -1
+        )
+      end
+    end
+
+    local width, height = calculate_dimensions()
+    vim.api.nvim_win_set_config(state.win_id, {
       width = width,
       height = height,
-      style = "minimal",
-      border = "rounded",
-      noautocmd = true,
     })
 
-    NVimUtils.set_win_options(state.win_id, {
-      cursorline = true,
-      scrolloff = 0,
-      winhl = table.concat({
-        "Normal:" .. config.highlights.normal,
-        "FloatBorder:Normal",
-        "CursorLine:" .. config.highlights.cursorline,
-        "FloatTitle:" .. config.highlights.title,
-      }, ","),
-    })
+    -- Freeze it back after updating the content
+    vim.api.nvim_set_option_value("modifiable", false, { buf = state.buf_id })
 
-    --- Set keymaps for navigation and selection
-    NVimUtils.add_keymaps({
-      ["<CR>"] = instance.select_item,
-      ["<Esc>"] = instance.hide,
-      ["j"] = function() move("down") end,
-      ["<Down>"] = function() move("down") end,
-      ["k"] = function() move("up") end,
-      ["<Up>"] = function() move("up") end,
-    }, {
-      mode = "n",
-      buf_id = state.buf_id,
-    })
+    -- Set cursor to the current item position
+    vim.api.nvim_win_set_cursor(state.win_id, { state.cursor_pos, 0 })
 
-    -- Close when loosing focus
-    vim.api.nvim_create_autocmd("BufLeave", {
-      buffer = state.buf_id,
-      callback = function() instance.hide() end,
-    })
+    -- Ensure the window's view starts from the first line
+    vim.api.nvim_win_call(state.win_id, function()
+      local view = vim.fn.winsaveview()
+      view.topline = 1 -- Set the top visible line to the first line
+      vim.fn.winrestview(view)
+    end)
+  end
 
-    render(config, state)
-  end)
+  --- Show the Select window.
+  function instance.show()
+    if state.is_visible then return end
+    if #state.items == 0 then
+      Logger.warn("Cannot render menu without any items.")
+      return
+    end
+    state.is_visible = true
 
-  --- Hide the select UI
-  instance.hide = vim.schedule_wrap(function()
+    if config.on_open then config.on_open() end
+
+    if not NVimUtils.is_buf_valid(state.buf_id) then
+      state.buf_id = NVimUtils.create_scratch_buffer({
+        buftype = "nofile",
+        bufhidden = "wipe",
+        buflisted = false,
+        swapfile = false,
+        modifiable = false,
+        filetype = "d41_ui_select",
+      })
+
+      setup_keymaps()
+    end
+
+    if not NVimUtils.is_win_valid(state.win_id) then
+      state.win_id = vim.api.nvim_open_win(
+        state.buf_id,
+        true,
+        vim.tbl_deep_extend("force", config.win_opts or {}, {
+          noautocmd = true,
+          title = config.title,
+          title_pos = config.title_pos,
+        })
+      )
+
+      NVimUtils.set_win_options(state.win_id, {
+        cursorline = true,
+        scrolloff = 0,
+        winhl = table.concat({
+          "Normal:" .. config.highlights.normal,
+          "FloatBorder:Normal",
+          "CursorLine:" .. config.highlights.cursorline,
+          "FloatTitle:" .. config.highlights.title,
+        }, ","),
+      })
+    end
+
+    -- Force initial position calculation
+    if state.cursor_pos == 0 then
+      state.cursor_pos = 1
+      if state.items[1].is_separator and #state.items > 1 then
+        state.cursor_pos = 2
+      end
+    end
+
+    render()
+    render_preview()
+  end
+
+  --- Close the select window and destroy vim resources.
+  function instance.hide()
     if NVimUtils.is_win_valid(state.win_id) then
       vim.api.nvim_win_close(state.win_id, true)
-      state.win_id = nil
-      state.buf_id = nil
     end
-  end)
+    if NVimUtils.is_buf_valid(state.buf_id) then
+      vim.api.nvim_buf_delete(state.buf_id, { force = true })
+    end
+    state.win_id = nil
+    state.buf_id = nil
+    state.is_visible = false
 
-  --- Refresh the select UI with new items
-  --- @param groups SelectUIItemGroup[]
-  instance.refresh = vim.schedule_wrap(function(groups)
-    state.items = groups and build_items_from_groups(groups) or state.items
-    render(config, state)
-  end)
+    if config.on_close then vim.schedule(function() config.on_close() end) end
+
+    if state.preview_ui then
+      state.preview_ui.hide()
+      state.preview_ui = nil
+    end
+  end
+
+  --- @param new_config SelectUIConfig
+  function instance.update(new_config)
+    config = vim.tbl_deep_extend("force", config, new_config)
+    if new_config.groups then
+      state.items = build_items_from_groups(new_config.groups)
+    end
+    if state.is_visible then
+      render()
+      render_preview()
+    end
+  end
 
   --- Handle item selection
-  instance.select_item = function()
+  function instance.select_item()
     local item = state.items[state.cursor_pos]
-    if not item or item.is_separator then return end
+    if not item or item.is_separator or item.is_selected then return end
 
-    if config.on_change then
-      vim.schedule(function() config.on_change(instance, item) end)
+    if config.on_select then
+      vim.schedule(function() config.on_select(instance, item) end)
     end
-    if config.should_close_on_change then instance.hide() end
+    if config.close_on_select then instance.hide() end
   end
 
   return instance
 end
 
-return SelectUI
+return M
