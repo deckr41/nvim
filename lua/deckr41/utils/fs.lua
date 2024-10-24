004a9a32da220c08b52183e0429a9ef8f4fae064
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2024-10-24 10:32:03 +0200

    feat(utils): add `get_folder_path`, `watch`, and `rg` functions to `fs.lua`
    - Add `get_folder_path` to extract folder from a file path
    - Add `watch` to monitor file changes with optional debounce
    - Add  `rg` for recursive file search using the `rg` command

--- lua/deckr41/utils/fs.lua
+++ lua/deckr41/utils/fs.lua
@@ -1,29 +1,62 @@
-local uv = vim.loop
+local FnUtils = require("deckr41.utils.fn")
+local Logger = require("deckr41.utils.loggr")("FSUtils")
 
 --- @class FSUtils
 local M = {}
 
--- Predicate function for determining if file exists
+--- Extract the folder from a file path
+--- @param file_path string
+--- @return string
+M.get_folder_path = function(file_path)
+  return vim.fn.fnamemodify(file_path, ":h")
+end
+
+--- @class FSWatchOpts
+--- @field callback function
+--- @field debounce_timeout integer?
+
+--- Watch and react to file changes
+--- @param file_path string
+--- @param opts FSWatchOpts
+M.watch = function(file_path, opts)
+  local watch = vim.uv.new_fs_event()
+
+  if not watch then
+    Logger.error("Failed to create file system watcher", { path = file_path })
+    return
+  end
+
+  local callback = nil
+  if opts.debounce_timeout then
+    callback = FnUtils.debounce(function() opts.callback(file_path) end)
+  else
+    callback = vim.schedule_wrap(function() opts.callback(file_path) end)
+  end
+
+  watch:start(file_path, {}, callback)
+end
+
+--- Predicate function for determining if file exists
 ---@param file_path string
 ---@return boolean
 M.does_file_exist = function(file_path)
-  return uv.fs_stat(file_path) and true or false
+  return vim.uv.fs_stat(file_path) and true or false
 end
 
--- Find all files mathching a name up the directory tree
+--- Find all files mathching a name up the directory tree
 --- @param opts {start_dir: string, match_files: string[]}
---- @return table<number, string>
+--- @return string[]
 M.find_up_sync = function(opts)
   local match_files = opts.match_files
   local start_dir = opts.start_dir
   local files = {}
 
-  -- Recursive function to scan up the tree
+  --- Recursive function to scan up the tree
   --- @param dir string
   local function find_up(dir)
     for _, match_file in ipairs(match_files) do
       local file_path = dir .. "/" .. match_file
-      if M.does_file_exist(file_path) then table.insert(files, file_path) end
+      if M.does_file_exist(file_path) then files[#files + 1] = file_path end
     end
 
     local parent = vim.fn.fnamemodify(dir, ":h")
@@ -35,13 +68,12 @@ M.find_up_sync = function(opts)
   return files
 end
 
--- Translate JSON file to a Lua table
---- @param file_path string Absolute path to the JSON file
---- @return table|nil
-M.read_json_as_table = function(file_path)
+--- Read JSON file as a Lua table
+--- @param file_path string
+--- @return table
+M.read_json = function(file_path)
   local file, err = io.open(file_path, "r")
-
-  if not file then error("Failed to open file: " .. err) end
+  if not file then Logger.error("Failed to open file", { path = file_path }) end
 
   --- @type string
   local content = file:read("*a")
@@ -50,7 +82,7 @@ M.read_json_as_table = function(file_path)
   return vim.fn.json_decode(content)
 end
 
---- Absolute path to where deckr41 plugin is installed
+--- Absolute path where deckr41 plugin is installed
 --- @return string
 M.get_plugin_path = function()
   local init_path = vim.api.nvim_get_runtime_file("lua/deckr41/init.lua", false)
@@ -67,7 +99,31 @@ M.copy_file = function(opts)
 
   if not should_overwrite and M.does_file_exist(to) then return end
 
-  uv.fs_copyfile(from, to)
+  vim.uv.fs_copyfile(from, to)
+end
+
+--- Recursively search for files using `rg`
+--- @param start_dir string
+--- @param opts { match_file_names: string[] }
+--- @return string[]
+M.rg = function(start_dir, opts)
+  local pattern = table.concat(opts.match_file_names, ",")
+  local cmd = string.format("rg --files --glob '{%s}' %s", pattern, start_dir)
+
+  local handle, error_msg = io.popen(cmd)
+  if not handle then
+    Logger.error("Something went wrong running `rg`", { rg = error_msg })
+  end
+
+  --- @type string
+  local result = handle:read("*a")
+  handle:close()
+
+  local files = {}
+  for file in result:gmatch("[^\r\n]+") do
+    files[#files + 1] = file
+  end
+  return files
 end
 
 return M
